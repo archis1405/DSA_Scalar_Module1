f3263f874be889b3cc8e96b2d5b1e9ceee0d8070
archis1405 <archismandas1405@gmail.com>
2025-09-27 02:04:41 +0530

    Just the first commit

---
+++ src/WaysToMakeFairArray.java
@@ -0,0 +1,14 @@
+public class WaysToMakeFairArray {
+
+    public int waysToMakeFair(int[] nums){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        WaysToMakeFairArray solution = new WaysToMakeFairArray();
+        int[] nums = {6,1,7,4,1};
+        int result = solution.waysToMakeFair(nums);
+        System.out.println("Number of ways to make the array fair: " + result);
+    }
+
+}
